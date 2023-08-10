b166f2c8e451594dc4d06b736377486260833b7c
Chaowapat Inthongkaew <chaowapat.i@gmail.com>
2023-08-10 17:49:07 +0700

    8f427dc

---
+++ _nuxt/6bd78a3.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{347:function(n,t,e){"use strict";e.r(t);var o={setup:function(){return{}}},c=e(58),component=Object(c.a)(o,(function(){return(0,this._self._c)("div",[this._v("\n    about\n")])}),[],!1,null,"5fe59f46",null);t.default=component.exports}}]);
